9a49df93334db56a8a8e98d0e0e8fe8fb91286ba
PjMitchell <peter@hilltop-digital.co.uk>
2015-10-04 15:45:51 +0100

    Added dictionary

--- Src/Collections.ts
+++ Src/Collections.ts
@@ -9,6 +9,21 @@
         getHashCode(): number;
     }
 
+    export class CollectionException implements Error{
+        name = "CollectionException";
+        constructor(public message: string) { }
+        toString() {
+            return this.name + ": " + this.message
+        }
+    }
+
+    export class KeyNotFoundException extends CollectionException {       
+        constructor(message: string) {
+            super(message)
+            name = "KeyNotFoundException";
+        }
+    };
+
     export class HashSet<T extends IEqualityComparable>{
         private buckets: T[][];
         private count: number;
@@ -53,7 +68,6 @@
             return result;
         }
 
-
         contains(item: T) {
             var hashCode = item.getHashCode();
             var bucket = this.buckets[hashCode];
@@ -71,5 +85,84 @@
             this.buckets = new Array<Array<T>>();
             this.count = 0;
         }
+    }
+
+    export class KeyValuePair<Tkey, TValue> {
+        constructor(public key: Tkey, public value: TValue) { }
+    }
+
+    export class Dictionary<Tkey extends IEqualityComparable, TValue> {
+        private buckets: KeyValuePair<Tkey, TValue>[][];
+        private count: number;
+
+        constructor() {
+            this.clear();
+        }
+        add(key: Tkey, value: TValue) {
+            var hashCode = key.getHashCode();
+            var bucket = this.buckets[hashCode];
+            if (bucket === undefined) {
+                var newBucket = new Array<KeyValuePair<Tkey, TValue>>();
+                newBucket.push(new KeyValuePair(key, value));
+                this.buckets[hashCode] = newBucket;
+                this.count = this.count + 1;
+                return;                
+            }
+            if (bucket.some((value) => value.key.equals(key)))
+                throw new CollectionException("Key already exists")    
+            bucket.push(new KeyValuePair(key, value));
+            this.count = this.count + 1;
+        }
+
+        get(key: Tkey) {
+            var hashCode = key.getHashCode();
+            var bucket = this.buckets[hashCode];
+            if (bucket === undefined) {
+                throw new KeyNotFoundException("Key not found");
+            }
+            var result = bucket.filter(value => value.key.equals(key)).pop();
+            if (bucket === undefined)
+                throw new KeyNotFoundException("Key not found");
+            return result.value;  
+        }
+
+        remove(item: Tkey) {
+            var hashCode = item.getHashCode();
+            var bucket = this.buckets[hashCode];
+            if (bucket === undefined) {
+                return false;
+            }
+            var result = false;
+            var newBucket = new Array<KeyValuePair<Tkey, TValue>>();
+            bucket.forEach((value) => {
+                if (!value.key.equals(item))
+                    newBucket.push(new KeyValuePair(value.key, value.value));
+                else
+                    result = true;
+            });
+            this.buckets[hashCode] = newBucket;
+            if (result)
+                this.count = this.count - 1;
+            return result;
+        }
+
+
+        containsKey(key: Tkey) {
+            var hashCode = key.getHashCode();
+            var bucket = this.buckets[hashCode];
+            if (bucket === undefined) {
+                return false;
+            }
+            return bucket.some((value) => value.key.equals(key)); 
+        }
+        clear() {
+            this.buckets = new Array<Array<KeyValuePair<Tkey, TValue>>>();
+            this.count = 0;
+        }
+
+        getCount() {
+            return this.count;
+        }
+
     }
 }
--- Tests/HashSetTests.ts
+++ Tests/HashSetTests.ts
@@ -2,92 +2,92 @@
 /// <reference path="../src/collections.ts" />
 /// <reference path="helpers/testitem.ts" />
 
-describe("HashSet", () => {
+describe("Dictionary", () => {
     describe("Contructor", () => {
     })
-    describe("Add", () => {
-        it("returns true if no items already added", () => {
-            var item1 = new TestClasses.TestItem(1);
-            var hashSet = new Collections.HashSet<TestClasses.TestItem>();
-            expect(hashSet.add(item1)).toEqual(true);
-        });
+    //describe("Add", () => {
+    //    it("returns true if no items already added", () => {
+    //        var item1 = new TestClasses.TestItem(1);
+    //        var hashSet = new Collections.HashSet<TestClasses.TestItem>();
+    //        expect(hashSet.add(item1)).toEqual(true);
+    //    });
 
-        it("returns false if equal item already added", () => {
-            var item1 = new TestClasses.TestItem(1);
-            var item2 = new TestClasses.TestItem(1);
+    //    it("returns false if equal item already added", () => {
+    //        var item1 = new TestClasses.TestItem(1);
+    //        var item2 = new TestClasses.TestItem(1);
 
-            var hashSet = new Collections.HashSet<TestClasses.TestItem>();
-            hashSet.add(item1);
-            expect(hashSet.add(item2)).toEqual(false);
-        });
-    })
-    describe("Contains", () => {
-        it("contains works", () => {
-            var item1 = new TestClasses.TestItem(1);
-            var item2 = new TestClasses.TestItem(2);
+    //        var hashSet = new Collections.HashSet<TestClasses.TestItem>();
+    //        hashSet.add(item1);
+    //        expect(hashSet.add(item2)).toEqual(false);
+    //    });
+    //})
+    //describe("Contains", () => {
+    //    it("contains works", () => {
+    //        var item1 = new TestClasses.TestItem(1);
+    //        var item2 = new TestClasses.TestItem(2);
 
-            var hashSet = new Collections.HashSet<TestClasses.TestItem>();
-            hashSet.add(item1);
-            expect(hashSet.contains(item1)).toEqual(true);
-            expect(hashSet.contains(item2)).toEqual(false);
-        });
-    })
+    //        var hashSet = new Collections.HashSet<TestClasses.TestItem>();
+    //        hashSet.add(item1);
+    //        expect(hashSet.contains(item1)).toEqual(true);
+    //        expect(hashSet.contains(item2)).toEqual(false);
+    //    });
+    //})
 
-    describe("GetCount", () => {
-        it("is zero on Construction", () => {
-            var hashSet = new Collections.HashSet<TestClasses.TestItem>();
-            expect(hashSet.getCount()).toEqual(0);
-        });
-        it("is adjusted on ItemAdded", () => {
-            var hashSet = new Collections.HashSet<TestClasses.TestItem>();
-            hashSet.add(new TestClasses.TestItem(3));
-            expect(hashSet.getCount()).toEqual(1);
-        });
-    })
+    //describe("GetCount", () => {
+    //    it("is zero on Construction", () => {
+    //        var hashSet = new Collections.HashSet<TestClasses.TestItem>();
+    //        expect(hashSet.getCount()).toEqual(0);
+    //    });
+    //    it("is adjusted on ItemAdded", () => {
+    //        var hashSet = new Collections.HashSet<TestClasses.TestItem>();
+    //        hashSet.add(new TestClasses.TestItem(3));
+    //        expect(hashSet.getCount()).toEqual(1);
+    //    });
+    //})
 
-    describe("Clear", () => {
-        it("removes Items", () => {
-            var hashSet = new Collections.HashSet<TestClasses.TestItem>();
-            var item1 = new TestClasses.TestItem(1);
-            hashSet.add(item1);
-            hashSet.clear();
-            expect(hashSet.contains(item1)).toEqual(false);
-        });
-        it("sets count to zero", () => {
-            var hashSet = new Collections.HashSet<TestClasses.TestItem>();
-            var item1 = new TestClasses.TestItem(1);
-            hashSet.add(item1);
-            hashSet.clear();
-            expect(hashSet.getCount()).toEqual(0);
-        });
-    })
+    //describe("Clear", () => {
+    //    it("removes Items", () => {
+    //        var hashSet = new Collections.HashSet<TestClasses.TestItem>();
+    //        var item1 = new TestClasses.TestItem(1);
+    //        hashSet.add(item1);
+    //        hashSet.clear();
+    //        expect(hashSet.contains(item1)).toEqual(false);
+    //    });
+    //    it("sets count to zero", () => {
+    //        var hashSet = new Collections.HashSet<TestClasses.TestItem>();
+    //        var item1 = new TestClasses.TestItem(1);
+    //        hashSet.add(item1);
+    //        hashSet.clear();
+    //        expect(hashSet.getCount()).toEqual(0);
+    //    });
+    //})
 
-    describe("remove", () => {
-        it("removes item from set", () => {
-            var hashSet = new Collections.HashSet<TestClasses.TestItem>();
-            var item1 = new TestClasses.TestItem(1);
-            hashSet.add(item1);
-            hashSet.remove(item1);
-            expect(hashSet.contains(item1)).toEqual(false);
-        });
-        it("return true if item removed from set", () => {
-            var hashSet = new Collections.HashSet<TestClasses.TestItem>();
-            var item1 = new TestClasses.TestItem(1);
-            hashSet.add(item1);
-            expect(hashSet.remove(item1)).toEqual(true);
-        });
-        it("return false if item not in set", () => {
-            var hashSet = new Collections.HashSet<TestClasses.TestItem>();
-            var item1 = new TestClasses.TestItem(1);
-            expect(hashSet.remove(item1)).toEqual(false);
-        });
+    //describe("remove", () => {
+    //    it("removes item from set", () => {
+    //        var hashSet = new Collections.HashSet<TestClasses.TestItem>();
+    //        var item1 = new TestClasses.TestItem(1);
+    //        hashSet.add(item1);
+    //        hashSet.remove(item1);
+    //        expect(hashSet.contains(item1)).toEqual(false);
+    //    });
+    //    it("return true if item removed from set", () => {
+    //        var hashSet = new Collections.HashSet<TestClasses.TestItem>();
+    //        var item1 = new TestClasses.TestItem(1);
+    //        hashSet.add(item1);
+    //        expect(hashSet.remove(item1)).toEqual(true);
+    //    });
+    //    it("return false if item not in set", () => {
+    //        var hashSet = new Collections.HashSet<TestClasses.TestItem>();
+    //        var item1 = new TestClasses.TestItem(1);
+    //        expect(hashSet.remove(item1)).toEqual(false);
+    //    });
 
-        it("remove reduces count", () => {
-            var hashSet = new Collections.HashSet<TestClasses.TestItem>();
-            var item1 = new TestClasses.TestItem(1);
-            hashSet.add(item1);
-            hashSet.remove(item1)
-            expect(hashSet.getCount()).toEqual(0);
-        });
-    })
+    //    it("remove reduces count", () => {
+    //        var hashSet = new Collections.HashSet<TestClasses.TestItem>();
+    //        var item1 = new TestClasses.TestItem(1);
+    //        hashSet.add(item1);
+    //        hashSet.remove(item1)
+    //        expect(hashSet.getCount()).toEqual(0);
+    //    });
+    //})
 })
